771c2db59912299dc0638e2b6fd7b0cbfddee912
Jarad Niemi <niemi@iastate.edu>
2015-11-13 10:43:55 -0600

    script to convert all

---
+++ manuscript/convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##
+## Copyright Michael I. Love and Rafael A. Irizarry  2015
+## (obtained from https://groups.google.com/forum/#!topic/leanpub/zF0u4ZGO6Jsa)
+## usage: ./tex2leanpub input.md output.md
+##
+
+./tex2leanpub.sh introduction.md introduction-converted.md
+./tex2leanpub.sh chapter1.md chapter1-converted.md
+./tex2leanpub.sh appendixA.md appendixA-converted.md
