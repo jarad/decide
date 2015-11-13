7d7573b8d6423fedab2d308cf4b870089befc0c5
Jarad Niemi <niemi@iastate.edu>
2015-11-13 11:05:28 -0600

    convert now loops over files

--- raw/convert.sh
+++ raw/convert.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+FILES=*.md
+for f in $FILES
+do 
+  echo "Converting $f"
+  ./tex2leanpub.sh $f ../manuscript/$f
+done
 
-##
-## Copyright Michael I. Love and Rafael A. Irizarry  2015
-## (obtained from https://groups.google.com/forum/#!topic/leanpub/zF0u4ZGO6Jsa)
-## usage: ./tex2leanpub input.md output.md
-##
+git add ../manuscript/*.md
+git commit -m "add converted files"
+git push
 
-./tex2leanpub.sh introduction.md introduction-converted.md
-./tex2leanpub.sh chapter1.md chapter1-converted.md
-./tex2leanpub.sh appendixA.md appendixA-converted.md
